a3d8f4fa4234aaeaf4b0863ef3950eeae821971d
Aleksey Averianov <Flex_d1@mail.ru>
2025-04-01 16:38:28 +0300

    Верстка главного окна

---
+++ src/components/StartWindowSlide.vue
@@ -0,0 +1,18 @@
+<script setup>
+  const props = defineProps({
+    srcImage: String,
+    titleText: String,
+  })
+
+</script>
+
+<template>
+  <div class="start-window__item">
+    <div class="start-window__item-img">
+      <img :src='srcImage' :alt='titleText'>
+    </div>
+    <div class="start-window__item-text">
+      {{ titleText }}
+    </div>
+  </div>
+</template>
--- src/components/TheStartWindow.vue
+++ src/components/TheStartWindow.vue
@@ -1,57 +1,57 @@
 <script setup>
-  import { register } from 'swiper/element/bundle';
-  register();
+  import { onMounted } from 'vue';
+  import StartWindowSlide from './StartWindowSlide.vue';
+  // swiper parameters
+  const swiperParams = {
+    breakpoints: {
+      360: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      980: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+    },
+    on: {
+      init() {
+        // ...
+      },
+    },
+  };
+
+  onMounted(() => {
+    const swiperEl = document.querySelector('swiper-container');
+    Object.assign(swiperEl, swiperParams);
+    swiperEl.initialize();
+  })
 
 </script>
 
 <template>
 
-  <div class="start-window bg-[url(@/assets/images/startWindow.jpg)] bg-no-repeat bg-cover overflow-hidden">
+  <div class="start-window bg-[url(@/assets/images/startWindow.jpg)] bg-no-repeat bg-cover overflow-hidden h-dvh">
 
-    <div class="start-window__wrapper max-w-[1600px] px-[20px] flex flex-col items-center ml-auto mr-auto">
+    <div class="start-window__wrapper max-w-[1600px] px-[20px] flex flex-col items-center ml-auto mr-auto mt-[120px] xl: mt-[100px]">
 
       <div class="start-window__anim-lists w-[100%]">
 
-        <swiper-container>
+        <swiper-container init="false">
           <swiper-slide>
-            <div class="start-window__item">
-              <div class="start-window__item-img">
-                <img src="@/assets/images/book1.jpg" alt="">
-              </div>
-              <div class="start-window__item-text">
-                Ваша личная книжная сокровищница
-              </div>
-            </div>
+            <StartWindowSlide src-image='/src/assets/images/book1.jpg' title-text='Ваша личная книжная сокровищница' />
           </swiper-slide>
           <swiper-slide>
-            <div class="start-window__item">
-              <div class="start-window__item-img">
-                <img src="@/assets/images/book2.jpg" alt="">
-              </div>
-              <div class="start-window__item-text">
-                Читайте с удовольствием
-              </div>
-            </div>
+            <StartWindowSlide src-image='/src/assets/images/book2.jpg' title-text='Читайте с удовольствием' />
           </swiper-slide>
           <swiper-slide>
-            <div class="start-window__item">
-              <div class="start-window__item-img">
-                <img src="@/assets/images/book3.jpg" alt="">
-              </div>
-              <div class="start-window__item-text">
-                Аккуратно и системно
-              </div>
-            </div>
+            <StartWindowSlide src-image='/src/assets/images/book3.jpg' title-text='Аккуратно и системно' />
           </swiper-slide>
           <swiper-slide>
-            <div class="start-window__item">
-              <div class="start-window__item-img">
-                <img src="@/assets/images/book4.jpg" alt="">
-              </div>
-              <div class="start-window__item-text">
-                Доступно на всех устройствах
-              </div>
-            </div>
+            <StartWindowSlide src-image='/src/assets/images/book4.jpg' title-text='Доступно на всех устройствах' />
           </swiper-slide>
         </swiper-container>
       </div>
